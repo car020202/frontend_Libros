5adc82159aab9e2ce259dd4ddb7f8f5e18644af5
car020202 <carlitosyanez9@gmail.com>
2024-11-19 11:34:15 -0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav style={{ backgroundColor: "#333", color: "#fff", padding: "10px" }}>
-      <h1>Navbar</h1>
+    <nav
+      style={{
+        backgroundColor: "#333",
+        color: "#fff",
+        padding: "10px",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "36px", margin: 0 }}>Libreria Kralo</h1>
     </nav>
   );
 };
